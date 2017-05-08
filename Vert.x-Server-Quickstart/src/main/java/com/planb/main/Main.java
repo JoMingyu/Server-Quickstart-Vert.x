a3cc39baafef43bb9f35bfb193d69e03b6da5130
JoMingyu <city7310@naver.com>
2017-05-08 11:02:52 +0900

    DB, Log 클래스 추가, Verticle 추가

--- Vert.x-Server-Quickstart/src/main/java/com/planb/main/Main.java
+++ Vert.x-Server-Quickstart/src/main/java/com/planb/main/Main.java
@@ -5,5 +5,6 @@ import io.vertx.core.Vertx;
 public class Main {
     public static void main(String[] args) {
     	Vertx vertx = Vertx.vertx();
+    	vertx.deployVerticle(new MainVerticle());
     }
 }
---
+++ Vert.x-Server-Quickstart/src/main/java/com/planb/main/MainVerticle.java
@@ -0,0 +1,18 @@
+package com.planb.main;
+
+import com.planb.support.Log;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+
+public class MainVerticle extends AbstractVerticle {
+	@Override
+	public void start() throws Exception {
+		
+	}
+	
+	@Override
+	public void stop(@SuppressWarnings("rawtypes") Future stopFuture) throws Exception {
+		Log.I("Server Stopped");
+	}
+}
