a7b98dea55f7f4600f245aadbdfbfa0b2263ff9e
PlanB <city7310@naver.com>
2017-05-09 15:21:11 +0900

    HttpClient 개조, Firebase 초기 구현

--- Vert.x-Server-Quickstart/src/main/java/com/planb/additional/Firebase.java
+++ Vert.x-Server-Quickstart/src/main/java/com/planb/additional/Firebase.java
@@ -2,12 +2,27 @@ package com.planb.additional;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.JSONObject;
 
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 
 public class Firebase {
+	private static final String SERVER_KEY = "";
+	private static final String API_URL = "https://fcm.googleapis.com/fcm/send";
+	private static HttpClientConfig config = null;
+	private static HttpClient client = null;
+	
 	public Firebase() {
+		config = new HttpClientConfig();
+		config.setTargetAddress(API_URL);
+		config.setReadTimeout(10000);
+		config.setConnectTimeout(10000);
+		client = new HttpClient();
+		
 		FirebaseOptions options;
 		try {
 			options = new FirebaseOptions.Builder()
@@ -19,5 +34,24 @@ public class Firebase {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	
+	public static void send(String message, String target) {
+		JSONObject body = new JSONObject();
+		body.put("body", message);
+		
+		JSONObject requestObject = new JSONObject();
+		requestObject.put("notification", body);
+		requestObject.put("to", target);
+		
+		Map<String, Object> headers = new HashMap<String, Object>();
+		headers.put("Authorization", "key=" + SERVER_KEY);
+		headers.put("Content-Type", "application/json");
+		
+		client.post("/", headers, requestObject);
+	}
+	
+	public static void sendByTopic() {
+		
 	}
 }
--- Vert.x-Server-Quickstart/src/main/java/com/planb/additional/HttpClient.java
+++ Vert.x-Server-Quickstart/src/main/java/com/planb/additional/HttpClient.java
@@ -4,11 +4,14 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
+
+import org.json.JSONObject;
 
 public class HttpClient {
 	private HttpClientConfig config = null;
@@ -17,14 +20,15 @@ public class HttpClient {
 	private HttpURLConnection connection = null;
 	private InputStream in = null;
 	private OutputStream out = null;
+	private OutputStreamWriter wr = null;
 	
 	public HttpClient() {
 		config = new HttpClientConfig();
 	}
 	
-	public int post(String uri) {
+	public int post(String uri, Map<String, Object> headers, Map<String, Object> params) {
 		/*
-		 * body data가 없는 post 요청
+		 * post 요청
 		 * status code 리턴
 		 */
 		String requestAddress = createRequestAddress(uri);
@@ -38,16 +42,28 @@ public class HttpClient {
 			connection.setReadTimeout(config.getReadTimeout());
 			connection.setConnectTimeout(config.getConnectTimeout());
 			
+			if(headers.size() > 0) {
+				for(String key : headers.keySet()) {
+					connection.setRequestProperty(key, (String) headers.get(key));
+				}
+			}
+			
+			if(params.size() > 0) {
+				out = connection.getOutputStream();
+				out.write(createParamBytes(params));
+				out.flush();
+			}
+			
 			return connection.getResponseCode();
-		} catch(IOException e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 			return 0;
 		}
 	}
 	
-	public int post(String uri, HashMap<String, Object> params) {
+	public int post(String uri, Map<String, Object> headers, JSONObject requestObject) {
 		/*
-		 * body data가 있는 post 요청
+		 * post 요청
 		 * status code 리턴
 		 */
 		String requestAddress = createRequestAddress(uri);
@@ -61,9 +77,15 @@ public class HttpClient {
 			connection.setReadTimeout(config.getReadTimeout());
 			connection.setConnectTimeout(config.getConnectTimeout());
 			
-			out = connection.getOutputStream();
-			out.write(createParamBytes(params));
-			// Body 데이터가 있으므로 바이트 형태의 데이터를 전송
+			if(headers.size() > 0) {
+				for(String key : headers.keySet()) {
+					connection.setRequestProperty(key, (String) headers.get(key));
+				}
+			}
+			
+			wr = new OutputStreamWriter(connection.getOutputStream());
+			wr.write(requestObject.toString());
+			wr.flush();
 			
 			return connection.getResponseCode();
 		} catch (IOException e) {
@@ -72,41 +94,18 @@ public class HttpClient {
 		}
 	}
 	
-	public HashMap<String, Object> get(String uri) {
+	public Map<String, Object> get(String uri, Map<String, Object> headers, Map<String, Object> params) {
 		/*
-		 * 파라미터가 없는 get 요청
+		 * get 요청
 		 * status code와 응답 데이터 리턴
 		 */
-		String requestAddress = createRequestAddress(uri);
-		// URI를 통해 요청 주소 얻어오기
-		try {
-			url = new URL(requestAddress);
-			connection = (HttpURLConnection) url.openConnection();
-			connection.setRequestMethod("GET");
-			connection.setReadTimeout(config.getReadTimeout());
-			connection.setConnectTimeout(config.getConnectTimeout());
-			
-			in = connection.getInputStream();
-			String response = getResponse(in);
-			// connection으로 얻은 InputStream에서 응답 얻어오기
-			Map<String, Object> map = new HashMap<String, Object>(1);
-			map.put("code", connection.getResponseCode());
-			map.put("response", response);
-			
-			return (HashMap<String, Object>) map;
-		} catch (IOException e) {
-			e.printStackTrace();
-			return null;
+		String requestAddress = null;
+		if(params.size() > 0) {
+			requestAddress = createRequestAddress(uri, params);
+			// URI와 파라미터를 통해 요청 주소 얻어오기
+		} else {
+			requestAddress = createRequestAddress(uri);
 		}
-	}
-	
-	public HashMap<String, Object> get(String uri, HashMap<String, Object> params) {
-		/*
-		 * 파라미터가 있는 get 요청
-		 * status code와 응답 데이터 리턴
-		 */
-		String requestAddress = createRequestAddress(uri, params);
-		// URI와 파라미터를 통해 요청 주소 얻어오기
 		try {
 			url = new URL(requestAddress);
 			connection = (HttpURLConnection) url.openConnection();
@@ -114,14 +113,24 @@ public class HttpClient {
 			connection.setReadTimeout(config.getReadTimeout());
 			connection.setConnectTimeout(config.getConnectTimeout());
 			
-			in = connection.getInputStream();
-			String response = getResponse(in);
-			// connection으로 얻은 InputStream에서 응답 얻어오기
-			Map<String, Object> map = new HashMap<String, Object>(1);
-			map.put("code", connection.getResponseCode());
-			map.put("response", response);
+			if(headers.size() > 0) {
+				for(String key : headers.keySet()) {
+					connection.setRequestProperty(key, (String) headers.get(key));
+				}
+			}
 			
-			return (HashMap<String, Object>) map;
+			Map<String, Object> map = new HashMap<String, Object>(1);
+			try {
+				in = connection.getInputStream();
+				String response = getResponse(in);
+				// connection으로 얻은 InputStream에서 응답 얻어오기
+				map.put("code", connection.getResponseCode());
+				map.put("response", response);
+			} catch(IOException e) {
+				map.put("code", 500);
+			}
+			
+			return map;
 		} catch(IOException e) {
 			e.printStackTrace();
 			return null;
@@ -140,7 +149,7 @@ public class HttpClient {
 		return config.getTargetAddress() + ":" + config.getTargetPort() + uri;
 	}
 	
-	private String createRequestAddress(String uri, HashMap<String, Object> params) {
+	private String createRequestAddress(String uri, Map<String, Object> params) {
 		/*
 		 * 파라미터가 있는 GET 요청에서의 request address
 		 * URI?key=value&key=value 형태
@@ -169,12 +178,8 @@ public class HttpClient {
 		return requestAddressStr;
 	}
 	
-	private byte[] createParamBytes(HashMap<String, Object> params) {
+	private byte[] createParamBytes(Map<String, Object> params) {
 		// POST 메소드에서 사용하는 byte 타입의 body 데이터
-		if(params.size() == 0) {
-			return "".getBytes();
-		}
-		
 		StringBuilder requestData = new StringBuilder();
 		
 		for(String key : params.keySet()) {
@@ -188,6 +193,9 @@ public class HttpClient {
 	}
 	
 	private String getResponse(InputStream in) {
+		if(in == null) {
+			return null;
+		}
 		ByteArrayOutputStream out = new ByteArrayOutputStream();
 		
 		byte[] buf = new byte[1024 * 8];
