b0556dc80a7a58ef05d87b28f481049bacbc78c4
JoMingyu <city7310@naver.com>
2017-06-01 16:02:10 +0900

    문서화 어노테이션, 데이터베이스 클래스 이름과 라우팅 시 문서화 방식 변경

--- Vert.x-Server-Quickstart/src/main/java/com/planb/restful/Sample.java
+++ Vert.x-Server-Quickstart/src/main/java/com/planb/restful/Sample.java
@@ -1,7 +1,7 @@
 package com.planb.restful;
 
-import com.planb.support.routing.Function;
-import com.planb.support.routing.RESTful;
+import com.planb.support.routing.API;
+import com.planb.support.routing.REST;
 import com.planb.support.routing.Route;
 
 import io.vertx.core.Handler;
@@ -9,8 +9,8 @@ import io.vertx.core.http.HttpMethod;
 import io.vertx.ext.web.RoutingContext;
 
 @Route(uri = "/sample", method = HttpMethod.POST)
-@Function(functionCategory = "기능 이름", summary = "요약")
-@RESTful(requestHeaders = "key : Type, key2 : Typ2", requestBody = "key : Type, key2 : Type2", successCode = 201, failureCode = 204)
+@API(functionCategory = "기능 이름", summary = "요약")
+@REST(requestHeaders = "key : Type, key2 : Typ2", requestBody = "key : Type, key2 : Type2", successCode = 201, failureCode = 204)
 public class Sample implements Handler<RoutingContext> {
 	@Override
 	public void handle(RoutingContext ctx) {
