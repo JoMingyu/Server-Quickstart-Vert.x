6620aa3754ada17e9158a9c15f768a22a8ed8f4c
JoMingyu <city7310@naver.com>
2017-06-12 23:24:23 +0900

    mysql-connector 6.0.6 적용

--- Vert.x-Server-Quickstart/src/main/java/com/planb/support/utilities/MySQL.java
+++ Vert.x-Server-Quickstart/src/main/java/com/planb/support/utilities/MySQL.java
@@ -9,13 +9,13 @@ import java.sql.SQLException;
 public class MySQL {
 	private static Connection connection;
 	
-	private static final String URL = "jdbc:mysql://localhost:3306/table_name";
+	private static final String URL = "jdbc:mysql://localhost:3306/table_name?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
 	private static final String USER = "";
 	private static final String PASSWORD = "";
 	
 	static {
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
+			Class.forName("com.mysql.cj.jdbc.Driver");
 			connection = DriverManager.getConnection(URL, USER, PASSWORD);
 		} catch (ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
